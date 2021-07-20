4377d71cb9e44169da310accf5dd8802e384dc34
vishakhshetty <57668627+vishakhshetty@users.noreply.github.com>
2021-07-20 16:40:11 +0530

    Add files via upload

---
+++ Singleton_Pattern/Singleton_Pattern/DBConn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Pattern
+{
+    public sealed class DBConn
+    {
+        private static DBConn instance;
+        private DBConn() 
+        {
+            Console.WriteLine("Single Instance created");
+        }
+        public static DBConn getInstance() 
+        {
+            if (instance==null) 
+            {
+                instance = new DBConn();
+            }
+            return instance;
+        }
+        public void display(string i) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Singleton_Pattern/Singleton_Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConn instance = DBConn.getInstance();
+            DBConn instance1 = DBConn.getInstance();
+            
+            instance.display("First Instance");
+            instance1.display("Second Instance");
+            
+            Console.ReadLine();
+        }
+    }
+}
